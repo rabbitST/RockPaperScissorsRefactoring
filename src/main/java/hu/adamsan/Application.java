39e518d81bcb217928e542f70de7b31136a12be2
rabbitST <biroattila.eu@gmail.com>
2022-08-30 18:55:24 +0200

    add refactored RockPaperScissors and ApplicationTest class.

--- src/main/java/hu/adamsan/Application.java
+++ src/main/java/hu/adamsan/Application.java
@@ -1,78 +1,76 @@
 package hu.adamsan;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Do you pick rock, paper, or scissors?");
-
-        Scanner choice = new Scanner(System.in);
-
-        String playerChoice = choice.nextLine();
-
-        // Making sure that the player enters either rock, paper, or scissors
-        // If they don't, move will not be recognized
-        if ("rock".equals(playerChoice) ||
-                "paper".equals(playerChoice) ||
-                "scissors".equals(playerChoice)) {
-        } else {
-            System.out.println("Move not recognized! Please try again.");
-        }
-
-        Random randomNumber = new Random();
-        // Random number from 0 - 98 (0-32, 33-65, 66-98)
-        int computerNumber = randomNumber.nextInt(98);
-
+        String playerChoice;
+        Application app = new Application();
+        do {
+            System.out.println("Do you pick rock, paper, or scissors?");
+            playerChoice = app.GetUserChoice();
+        } while (!app.CheckInput(playerChoice))
+                ;
         // Computer chooses a move based on a randomly generated int
-        // 0-32 = rock
-        // 33-66 = paper
-        // 66-98 = scissors
-        String computerMoveChoice;
-        if (computerNumber < 32) {
-            computerMoveChoice = "rock";
-        } else if (computerNumber > 32 && computerNumber < 66) {
-            computerMoveChoice = "paper";
-        } else {
-            computerMoveChoice = "scissors";
-        }
+       String computerMoveChoice=app.ComputerMove();
 
         // Printing out what the computer chose
-        System.out.println("The computer chose: " + computerMoveChoice);
+        System.out.println("The computer chose: "+ computerMoveChoice);
 
         // Comparing the choices
         // Both choices are the same (tie)
-        if (playerChoice == computerMoveChoice) {
-            System.out.println("It's a tie!");
-        }
-        // Player choose rock --- Computer choose paper (computer wins)
-        else if (playerChoice == "rock" && computerMoveChoice == "paper") {
-            System.out.println("Paper beats rock. The computer wins!");
+        PrintIt(app.CheckWinner(playerChoice,computerMoveChoice));
+    }
 
-        }
-        // Player choose rock --- Computer choose scissors (player wins)
-        else if (playerChoice == "rock" && computerMoveChoice == "scissors") {
-            System.out.println("Rock beats scissors. Player wins!");
-        }
+    public String GetUserChoice() {
+        Scanner choice = new Scanner(System.in);
+        return choice.nextLine();
+    }
 
-        // Player choose paper --- Computer choose rock (player wins)
-        else if (playerChoice == "paper" && computerMoveChoice == "rock") {
-            System.out.println("Paper beats rock. Player wins!");
+    public boolean CheckInput(String playerChoice) {
+        String[] userOptions = new String[]{"rock", "paper", "scissors"};
+        if (!Arrays.asList(userOptions).contains(playerChoice)) {
+            System.out.println("Move not recognized! Please try again.");
+            return false;
         }
-        // Player choose paper --- Computer choose scissors (computer wins)
-        else if (playerChoice == "paper" && computerMoveChoice == "scissors") {
-            System.out.println("Scissors beats paper. The computer wins!");
-        }
+        return true;
+    }
 
-        // Player choose scissors --- Computer choose rock (computer wins)
-        else if (playerChoice == "scissors" && computerMoveChoice == "rock") {
-            System.out.println("Rock beats scissors. The computer wins!");
+    public String ComputerMove() {
+        Random randomNumber = new Random();
+        String[] computerOptions = new String[]{"rock", "paper", "scissors"};
+        return computerOptions[randomNumber.nextInt(3)];
+    }
+    public List CheckWinner(String player, String computer) {
+        List<String> winner= new ArrayList<>();
+        List<String> moves=new ArrayList<>(){{
+            add(player);add(computer);
+        }};
+        if(moves.contains("rock") && moves.contains("scissors")){
+            winner.add("1");
+            winner.add(moves.indexOf("rock")==0?"Player":"Computer");
         }
+        if(moves.contains("paper") && moves.contains("rock")){
+            winner.add("2");
+            winner.add(moves.indexOf("paper")==0?"Player":"Computer");
+        }
+        if(moves.contains("scissors") && moves.contains("paper")){
+            winner.add("3");
+            winner.add(moves.indexOf("scissors")==0?"Player":"Computer");
+        }
+        if(player.equals(computer)){
+            winner.add("4");
+        }
+        return winner;
+    }
 
-        // Player choose scissors --- Computer choose paper (player wins)
-        else {
-            System.out.println("Scissors beats paper. Player wins!");
+    private static void PrintIt(List<String> CheckWinner) {
+        switch (CheckWinner.get(0)){
+            case "1"-> System.out.printf("\nRock beats scissors.The %s wins.",CheckWinner.get(1));
+            case "2"-> System.out.printf("\nPaper beats rock.The %s wins.",CheckWinner.get(1));
+            case "3"-> System.out.printf("\nScissors beats paper.The %s wins.",CheckWinner.get(1));
+            case "4"-> System.out.println("It's a tie!");
         }
     }
 }
--- src/test/java/hu/adamsan/ApplicationTest.java
+++ src/test/java/hu/adamsan/ApplicationTest.java
@@ -1,4 +1,42 @@
+package hu.adamsan;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ApplicationTest {
-  
+    Application appTest = new Application();
+    List<String> options=new ArrayList<>(){{
+        add("rock");add("paper");add("scissors");
+    }};
+
+
+
+    @Test
+    void checkInputTrue() {
+        assertTrue(appTest.CheckInput("scissors"));
+        assertTrue(appTest.CheckInput("rock"));
+        assertTrue(appTest.CheckInput("paper"));
+        //assertTrue(appTest.CheckInput("12458"));
+    }
+    @Test
+    void checkInputFalse() {
+        assertFalse(appTest.CheckInput("12458"));
+    }
+
+    @Test
+    void ComputerMove() {
+        assertTrue(options.contains(appTest.ComputerMove()));
+        assertTrue(options.contains(appTest.ComputerMove()));
+        assertTrue(options.contains(appTest.ComputerMove()));
+    }
+
+    @Test
+    void checkWinner() {
+        assertEquals(appTest.CheckWinner("rock","scissors").get(1),"Player");
+        assertEquals(appTest.CheckWinner("rock","paper").get(1),"Computer");
+        assertEquals(appTest.CheckWinner("scissors","paper").get(1),"Player");
+    }
 }
